915cf166d99fdc26b566317470e6b6ed32971fa8
Luke Sweeney <llamicron@gmail.com>
2018-09-06 21:10:22 -0500

    adds basic docopt interface in brewkit

---
+++ brewkit/__init__.py
@@ -0,0 +1,27 @@
+"""
+Usage:
+    brewkit
+    brewkit api
+    brewkit interface
+
+Options:
+  -h --help     Show this screen.
+  --version     Show version.
+"""
+
+from docopt import docopt
+
+def main():
+    args = docopt(__doc__)
+
+    if args['interface']:
+        print('starting interface')
+
+    elif args['api']:
+        print('starting API')
+
+    else:
+        print('starting CLI')
+
+if __name__ == '__main__':
+    main()
---
+++ brewkit/tests/test_basic.py
@@ -0,0 +1,6 @@
+import unittest
+
+
+class TestBasic(unittest.TestCase):
+    def test_basic(self):
+        assert True
