8775fea08042f14aae024ebd9e497e2177b5f5c2
Luke Sweeney <llamicron@gmail.com>
2018-09-11 18:58:47 -0500

    Adds logger

---
+++ app/log.py
@@ -0,0 +1,10 @@
+import os
+
+import logzero
+from logzero import logger
+
+logfile = os.path.expanduser('~') + "/brewkit.log"
+logzero.logfile(logfile, maxBytes=1e6, backupCount=3)
+
+log = logger
+# from log import log
