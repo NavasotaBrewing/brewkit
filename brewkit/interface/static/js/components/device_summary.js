fa290c451a8970bd0e992131019b6eb284d00623
Luke Sweeney <llamicron@gmail.com>
2018-12-22 19:25:59 -0600

    Adds device summary

---
+++ brewkit/interface/static/js/components/device_summary.js
@@ -0,0 +1,133 @@
+// Don't use this directly, use DeviceSummary with :compact="true"
+var CompactDeviceSummaryComponent = Vue.component('compact-device-summary', {
+  props: {
+    devices: {
+      type: Array,
+      required: true,
+    },
+    editable: {
+      type: Boolean,
+      required: false,
+      default: true
+    }
+  },
+  template: `
+    <div id="compactDeviceSummary" class="uk-overflow-auto">
+      <table class="uk-table uk-table-striped uk-table-small uk-table-middle">
+        <caption>
+          <h4>
+            Devices
+          </h4>
+        </caption>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Temperature</th>
+            <th class="uk-float-right">State</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="device in devices">
+            <!-- Name -->
+            <td>{{ device.name }}</td>
+            <!-- Temp input -->
+            <td>
+              <div v-if="device.type == 'thermostat'" class="uk-width-1-3@s">
+                <input :disabled="!editable" type="tel" v-model="device.sv" class="uk-input uk-form-small"
+                  placeholder="Target ˚">
+              </div>
+            </td>
+            <!-- State -->
+            <td class="uk-align-right">
+              <span v-if="editable" @click="device.state = Math.abs(device.state - 1)" class="uk-label cursor" :class="{ 'uk-label-success': device.state, 'uk-label-danger': !device.state }">
+                {{ device.states[device.state ? 1 : 0] }}
+              </span>
+              <span v-else class="uk-label" :class="{ 'uk-label-success': device.state, 'uk-label-danger': !device.state }">
+                {{ device.states[device.state ? 1 : 0] }}
+              </span>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  `
+})
+
+
+var DeviceSummaryComponent = Vue.component('device-summary', {
+  props: {
+    devices: {
+      type: Array,
+      required: true
+    },
+    editable: {
+      type: Boolean,
+      required: false,
+      default: true,
+    },
+    'include-thermos': {
+      type: Boolean,
+      required: false,
+      default: true
+    },
+    compact: {
+      type: Boolean,
+      required: false,
+      default: false
+    }
+  },
+  computed: {
+    valves: function() {
+      return this.devices.filter(d => d.type != 'thermostat')
+    },
+    thermos: function() {
+      return this.devices.filter(d => d.type == 'thermostat')
+    }
+  },
+  components: {
+    'compact-device-summary': CompactDeviceSummaryComponent,
+  },
+  template: `
+    <div>
+      <div v-if="compact">
+        <compact-device-summary v-if="includeThermos" :editable="editable" :devices="devices" ></compact-device-summary>
+        <compact-device-summary v-else :editable="editable" :devices="valves"></compact-device-summary>
+      </div>
+      <div v-if="!(valves.length < 1 && !includeThermos) && !compact" id="deviceSummary" uk-filter="target: #deviceFilter">
+        <ul class="uk-subnav uk-subnav-pill">
+          <li uk-filter-control=".device" class="uk-active"><a href="#">All</a></li>
+          <li uk-filter-control=".tag-onOff"><a href="#">On/Off</a></li>
+          <li uk-filter-control=".tag-divert"><a href="#">Diverts</a></li>
+          <li uk-filter-control=".tag-pump"><a href="#">Pumps</a></li>
+          <li v-if="includeThermos" uk-filter-control=".tag-thermostat"><a href="#">Thermostats</a></li>
+        </ul>
+
+        <ul id="deviceFilter" class="uk-child-width-1-2@s uk-child-width-1-3@l" uk-grid="masonry: true">
+          <!-- Device Loop -->
+          <li v-for="device in devices" v-if="device.type != 'thermostat' || includeThermos" class="device" :class="'tag-' + device.type">
+            <div class="uk-card uk-card-default uk-card-body">
+              <div class="uk-grid-divider uk-child-width-expand@s" uk-grid>
+                <div>
+                  <dl>
+                    <dt>{{ device.name }}</dt>
+                    <span v-if="device.state == 0" class="uk-label uk-label-danger">{{ device.states[device.state ? 1 : 0] }}</span>
+                    <span v-if="device.state == 1" class="uk-label uk-label-success">{{ device.states[device.state ? 1 : 0] }}</span>
+                  </dl>
+                </div>
+                <div>
+                  <div class="uk-container uk-position-center uk-margin-left">
+                    <!-- New Target only for thermostats -->
+                    <div v-if="device.type == 'thermostat'" class="uk-margin-small">
+                      <input type="text" :disabled="!editable" class="uk-input uk-form-small" v-model="device.sv" placeholder="Target">
+                    </div>
+                    <button @click="device.state = !device.state" :disabled="!editable" class="uk-button enactor uk-button-primary">Toggle</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  `
+})
--- brewkit/interface/static/js/procedures.js
+++ brewkit/interface/static/js/procedures.js
@@ -6,7 +6,7 @@ test_proc = {
       index: 0,
       name: 'Start',
       id: guid(),
-      comments: 'Just the first step. Just the first step. Just the first step. Just the first step. Just the first step. Just the first step. Just the first step. Just the first step. Just the first step. Just the first step. Just the first step. Just the first step. Just the first step. Just the first step. Just the first step. ',
+      comments: 'Just the first step.',
       config: JSON.parse('{"id":"k2gweii8hwerwktt1birnb","name":"Complete","slackWebhook":"https://hooks.slack.com/services/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","slackChannel":"@luke","devices":[{"name":"HLT Valve","controller_address":"2","state":1,"address":"0","type":"onOff","id":"kebi2wrtr1tiw8ikbghwen","states":{"0":"Closed","1":"Open"}},{"name":"HLT Divert","controller_address":"2","state":0,"address":"1","type":"divert","id":"iwieb81ktkbwrwi2nrehgt","states":{"0":"Boil","1":"Mash"}},{"name":"RIMS Divert","controller_address":"2","state":1,"address":"2","type":"divert","id":"gwbnkreerk8iwi12withbt","states":{"0":"Boil","1":"RIMS"}},{"name":"RIMS Pump","controller_address":"2","state":1,"address":"3","type":"pump","id":"rwtiebb2iwwtikk1nreg8h","states":{"0":"Off","1":"On"}},{"name":"RIMS Thermostat","controller_address":"1","state":0,"address":"","type":"thermostat","id":"kwn8ehikir1riwgwtteb2b","states":{"0":"Off","1":"On"},"sv":150,"pv":112.8}]}')
     },
     {
@@ -120,7 +120,9 @@ let proc = new Vue({
     procSelect: '',
     procs: [test_proc],
     proc: test_proc,
-    step: {}
+    step: {},
+    showTitleInput: false,
+    showCommentsInput: false
   },
   methods: {
     selectConfig(config) {
@@ -147,7 +149,7 @@ let proc = new Vue({
 
     saveProc() {
       console.log('saved')
-    }
+    },
   },
   watch: {
     procSelect: function() {
