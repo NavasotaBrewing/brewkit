18a44c1ae8afef76646ec9f97462753dd80d9235
Luke Sweeney <llamicron@gmail.com>
2018-12-23 01:06:50 -0600

    More work on the procedures page, adds reindexer

--- brewkit/interface/static/js/components/device_summary.js
+++ brewkit/interface/static/js/components/device_summary.js
@@ -88,43 +88,48 @@ var DeviceSummaryComponent = Vue.component('device-summary', {
     }
   },
   template: `
-    <div v-if="!(valves.length < 1 && !includeThermos) && !compact" id="deviceSummary" uk-filter="target: #deviceFilter">
-      <ul class="uk-subnav uk-subnav-pill">
-        <li uk-filter-control=".device" class="uk-active"><a href="#">All</a></li>
-        <li uk-filter-control=".tag-onOff"><a href="#">On/Off</a></li>
-        <li uk-filter-control=".tag-divert"><a href="#">Diverts</a></li>
-        <li uk-filter-control=".tag-pump"><a href="#">Pumps</a></li>
-        <li v-if="includeThermos" uk-filter-control=".tag-thermostat"><a href="#">Thermostats</a></li>
-      </ul>
+    <div>
+      <div v-if="compact">
+        <compact-device-summary :devices="devices" :editable="editable" :include-thermos="includeThermos"></compact-device-summary>
+      </div>
+      <div v-if="!(valves.length < 1 && !includeThermos) && !compact" id="deviceSummary" uk-filter="target: #deviceFilter">
+        <ul class="uk-subnav uk-subnav-pill">
+          <li uk-filter-control=".device" class="uk-active"><a href="#">All</a></li>
+          <li uk-filter-control=".tag-onOff"><a href="#">On/Off</a></li>
+          <li uk-filter-control=".tag-divert"><a href="#">Diverts</a></li>
+          <li uk-filter-control=".tag-pump"><a href="#">Pumps</a></li>
+          <li v-if="includeThermos" uk-filter-control=".tag-thermostat"><a href="#">Thermostats</a></li>
+        </ul>
 
-      <ul id="deviceFilter" class="uk-child-width-1-2@s uk-child-width-1-3@l" uk-grid="masonry: true">
-        <!-- Device Loop -->
-        <li v-for="device in devices" v-if="device.type != 'thermostat' || includeThermos" class="device"
-          :class="'tag-' + device.type">
-          <div class="uk-card uk-card-default uk-card-body">
-            <div class="uk-grid-divider uk-child-width-expand@s" uk-grid>
-              <div>
-                <dl>
-                  <dt>{{ device.name }}</dt>
-                  <span @click="device.state = !device.state" v-if="device.state == 0" class="cursor enactor uk-label uk-label-danger">{{
-                    device.states[device.state ? 1 : 0]
-                    }}</span>
-                  <span @click="device.state = !device.state" v-if="device.state == 1" class="cursor enactor uk-label uk-label-success">{{
-                    device.states[device.state ? 1 : 0]
-                    }}</span>
-                  <!-- New Target only for thermostats -->
-                  <div v-if="device.type == 'thermostat'" class="uk-margin-small">
-                  <div class="uk-inline">
-                    <a class="uk-form-icon uk-form-icon-flip" @click="setSv(device)" uk-icon="icon: check"></a>
-                    <input :id="'newSv' + device.id" @keydown.enter="setSv(device)"  class="uk-input uk-form-small" type="text" placeholder="New SV">
-                  </div>
-                  </div>
-                </dl>
+        <ul id="deviceFilter" class="uk-child-width-1-2@s uk-child-width-1-3@l" uk-grid="masonry: true">
+          <!-- Device Loop -->
+          <li v-for="device in devices" v-if="device.type != 'thermostat' || includeThermos" class="device"
+            :class="'tag-' + device.type">
+            <div class="uk-card uk-card-default uk-card-body">
+              <div class="uk-grid-divider uk-child-width-expand@s" uk-grid>
+                <div>
+                  <dl>
+                    <dt>{{ device.name }}</dt>
+                    <span @click="device.state = !device.state" v-if="device.state == 0" class="cursor enactor uk-label uk-label-danger">{{
+                      device.states[device.state ? 1 : 0]
+                      }}</span>
+                    <span @click="device.state = !device.state" v-if="device.state == 1" class="cursor enactor uk-label uk-label-success">{{
+                      device.states[device.state ? 1 : 0]
+                      }}</span>
+                    <!-- New Target only for thermostats -->
+                    <div v-if="device.type == 'thermostat'" class="uk-margin-small">
+                    <div class="uk-inline">
+                      <a class="uk-form-icon uk-form-icon-flip" @click="setSv(device)" uk-icon="icon: check"></a>
+                      <input :id="'newSv' + device.id" @keydown.enter="setSv(device)"  class="uk-input uk-form-small" type="text" placeholder="New SV">
+                    </div>
+                    </div>
+                  </dl>
+                </div>
               </div>
             </div>
-          </div>
-        </li>
-      </ul>
+          </li>
+        </ul>
+      </div>
     </div>
   `
 })
--- brewkit/interface/static/js/procedures.js
+++ brewkit/interface/static/js/procedures.js
@@ -4,123 +4,32 @@ test_proc = {
   steps: [
     {
       index: 0,
+      tools: {
+        delay: {
+          minutes: '',
+          seconds: ''
+        },
+      },
       name: 'Start',
       id: guid(),
       comments: 'Just the first step.',
-      config: JSON.parse('{"id":"k2gweii8hwerwktt1birnb","name":"Complete","slackWebhook":"https://hooks.slack.com/services/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","slackChannel":"@luke","devices":[{"name":"HLT Valve","controller_address":"2","state":1,"address":"0","type":"onOff","id":"kebi2wrtr1tiw8ikbghwen","states":{"0":"Closed","1":"Open"}},{"name":"HLT Divert","controller_address":"2","state":0,"address":"1","type":"divert","id":"iwieb81ktkbwrwi2nrehgt","states":{"0":"Boil","1":"Mash"}},{"name":"RIMS Divert","controller_address":"2","state":1,"address":"2","type":"divert","id":"gwbnkreerk8iwi12withbt","states":{"0":"Boil","1":"RIMS"}},{"name":"RIMS Pump","controller_address":"2","state":1,"address":"3","type":"pump","id":"rwtiebb2iwwtikk1nreg8h","states":{"0":"Off","1":"On"}},{"name":"RIMS Thermostat","controller_address":"1","state":0,"address":"","type":"thermostat","id":"kwn8ehikir1riwgwtteb2b","states":{"0":"Off","1":"On"},"sv":150,"pv":112.8}]}')
-    },
-    {
-      index: 1,
-      name: 'First Step',
-      id: guid(),
-      comments: 'Closes the HLT Valve',
-      config: JSON.parse('{"id":"k2gweii8hwerwktt1birnb","name":"Complete","slackWebhook":"https://hooks.slack.com/services/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","slackChannel":"@luke","devices":[{"name":"HLT Valve","controller_address":"2","state":1,"address":"0","type":"onOff","id":"kebi2wrtr1tiw8ikbghwen","states":{"0":"Closed","1":"Open"}},{"name":"HLT Divert","controller_address":"2","state":0,"address":"1","type":"divert","id":"iwieb81ktkbwrwi2nrehgt","states":{"0":"Boil","1":"Mash"}},{"name":"RIMS Divert","controller_address":"2","state":1,"address":"2","type":"divert","id":"gwbnkreerk8iwi12withbt","states":{"0":"Boil","1":"RIMS"}},{"name":"RIMS Pump","controller_address":"2","state":1,"address":"3","type":"pump","id":"rwtiebb2iwwtikk1nreg8h","states":{"0":"Off","1":"On"}},{"name":"RIMS Thermostat","controller_address":"1","state":0,"address":"","type":"thermostat","id":"kwn8ehikir1riwgwtteb2b","states":{"0":"Off","1":"On"},"sv":150,"pv":112.8}]}')
-    },
-    {
-      index: 2,
-      name: 'Set sparge temp',
-      id: guid(),
-      comments: 'Sets the sparge temp to 173',
-      config: JSON.parse('{"id":"k2gweii8hwerwktt1birnb","name":"Complete","slackWebhook":"https://hooks.slack.com/services/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","slackChannel":"@luke","devices":[{"name":"HLT Valve","controller_address":"2","state":1,"address":"0","type":"onOff","id":"kebi2wrtr1tiw8ikbghwen","states":{"0":"Closed","1":"Open"}},{"name":"HLT Divert","controller_address":"2","state":0,"address":"1","type":"divert","id":"iwieb81ktkbwrwi2nrehgt","states":{"0":"Boil","1":"Mash"}},{"name":"RIMS Divert","controller_address":"2","state":1,"address":"2","type":"divert","id":"gwbnkreerk8iwi12withbt","states":{"0":"Boil","1":"RIMS"}},{"name":"RIMS Pump","controller_address":"2","state":1,"address":"3","type":"pump","id":"rwtiebb2iwwtikk1nreg8h","states":{"0":"Off","1":"On"}},{"name":"RIMS Thermostat","controller_address":"1","state":0,"address":"","type":"thermostat","id":"kwn8ehikir1riwgwtteb2b","states":{"0":"Off","1":"On"},"sv":150,"pv":112.8}]}')
-    },
-    {
-      index: 0,
-      name: 'Start',
-      id: guid(),
-      comments: 'Just the first step',
-      config: JSON.parse('{"id":"k2gweii8hwerwktt1birnb","name":"Complete","slackWebhook":"https://hooks.slack.com/services/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","slackChannel":"@luke","devices":[{"name":"HLT Valve","controller_address":"2","state":1,"address":"0","type":"onOff","id":"kebi2wrtr1tiw8ikbghwen","states":{"0":"Closed","1":"Open"}},{"name":"HLT Divert","controller_address":"2","state":0,"address":"1","type":"divert","id":"iwieb81ktkbwrwi2nrehgt","states":{"0":"Boil","1":"Mash"}},{"name":"RIMS Divert","controller_address":"2","state":1,"address":"2","type":"divert","id":"gwbnkreerk8iwi12withbt","states":{"0":"Boil","1":"RIMS"}},{"name":"RIMS Pump","controller_address":"2","state":1,"address":"3","type":"pump","id":"rwtiebb2iwwtikk1nreg8h","states":{"0":"Off","1":"On"}},{"name":"RIMS Thermostat","controller_address":"1","state":0,"address":"","type":"thermostat","id":"kwn8ehikir1riwgwtteb2b","states":{"0":"Off","1":"On"},"sv":150,"pv":112.8}]}')
-    },
-    {
-      index: 1,
-      name: 'First Step',
-      id: guid(),
-      comments: 'Closes the HLT Valve',
-      config: JSON.parse('{"id":"k2gweii8hwerwktt1birnb","name":"Complete","slackWebhook":"https://hooks.slack.com/services/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","slackChannel":"@luke","devices":[{"name":"HLT Valve","controller_address":"2","state":1,"address":"0","type":"onOff","id":"kebi2wrtr1tiw8ikbghwen","states":{"0":"Closed","1":"Open"}},{"name":"HLT Divert","controller_address":"2","state":0,"address":"1","type":"divert","id":"iwieb81ktkbwrwi2nrehgt","states":{"0":"Boil","1":"Mash"}},{"name":"RIMS Divert","controller_address":"2","state":1,"address":"2","type":"divert","id":"gwbnkreerk8iwi12withbt","states":{"0":"Boil","1":"RIMS"}},{"name":"RIMS Pump","controller_address":"2","state":1,"address":"3","type":"pump","id":"rwtiebb2iwwtikk1nreg8h","states":{"0":"Off","1":"On"}},{"name":"RIMS Thermostat","controller_address":"1","state":0,"address":"","type":"thermostat","id":"kwn8ehikir1riwgwtteb2b","states":{"0":"Off","1":"On"},"sv":150,"pv":112.8}]}')
-    },
-    {
-      index: 2,
-      name: 'Set sparge temp',
-      id: guid(),
-      comments: 'Sets the sparge temp to 173',
-      config: JSON.parse('{"id":"k2gweii8hwerwktt1birnb","name":"Complete","slackWebhook":"https://hooks.slack.com/services/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","slackChannel":"@luke","devices":[{"name":"HLT Valve","controller_address":"2","state":1,"address":"0","type":"onOff","id":"kebi2wrtr1tiw8ikbghwen","states":{"0":"Closed","1":"Open"}},{"name":"HLT Divert","controller_address":"2","state":0,"address":"1","type":"divert","id":"iwieb81ktkbwrwi2nrehgt","states":{"0":"Boil","1":"Mash"}},{"name":"RIMS Divert","controller_address":"2","state":1,"address":"2","type":"divert","id":"gwbnkreerk8iwi12withbt","states":{"0":"Boil","1":"RIMS"}},{"name":"RIMS Pump","controller_address":"2","state":1,"address":"3","type":"pump","id":"rwtiebb2iwwtikk1nreg8h","states":{"0":"Off","1":"On"}},{"name":"RIMS Thermostat","controller_address":"1","state":0,"address":"","type":"thermostat","id":"kwn8ehikir1riwgwtteb2b","states":{"0":"Off","1":"On"},"sv":150,"pv":112.8}]}')
-    },
-    {
-      index: 0,
-      name: 'Start',
-      id: guid(),
-      comments: 'Just the first step',
-      config: JSON.parse('{"id":"k2gweii8hwerwktt1birnb","name":"Complete","slackWebhook":"https://hooks.slack.com/services/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","slackChannel":"@luke","devices":[{"name":"HLT Valve","controller_address":"2","state":1,"address":"0","type":"onOff","id":"kebi2wrtr1tiw8ikbghwen","states":{"0":"Closed","1":"Open"}},{"name":"HLT Divert","controller_address":"2","state":0,"address":"1","type":"divert","id":"iwieb81ktkbwrwi2nrehgt","states":{"0":"Boil","1":"Mash"}},{"name":"RIMS Divert","controller_address":"2","state":1,"address":"2","type":"divert","id":"gwbnkreerk8iwi12withbt","states":{"0":"Boil","1":"RIMS"}},{"name":"RIMS Pump","controller_address":"2","state":1,"address":"3","type":"pump","id":"rwtiebb2iwwtikk1nreg8h","states":{"0":"Off","1":"On"}},{"name":"RIMS Thermostat","controller_address":"1","state":0,"address":"","type":"thermostat","id":"kwn8ehikir1riwgwtteb2b","states":{"0":"Off","1":"On"},"sv":150,"pv":112.8}]}')
-    },
-    {
-      index: 1,
-      name: 'First Step',
-      id: guid(),
-      comments: 'Closes the HLT Valve',
-      config: JSON.parse('{"id":"k2gweii8hwerwktt1birnb","name":"Complete","slackWebhook":"https://hooks.slack.com/services/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","slackChannel":"@luke","devices":[{"name":"HLT Valve","controller_address":"2","state":1,"address":"0","type":"onOff","id":"kebi2wrtr1tiw8ikbghwen","states":{"0":"Closed","1":"Open"}},{"name":"HLT Divert","controller_address":"2","state":0,"address":"1","type":"divert","id":"iwieb81ktkbwrwi2nrehgt","states":{"0":"Boil","1":"Mash"}},{"name":"RIMS Divert","controller_address":"2","state":1,"address":"2","type":"divert","id":"gwbnkreerk8iwi12withbt","states":{"0":"Boil","1":"RIMS"}},{"name":"RIMS Pump","controller_address":"2","state":1,"address":"3","type":"pump","id":"rwtiebb2iwwtikk1nreg8h","states":{"0":"Off","1":"On"}},{"name":"RIMS Thermostat","controller_address":"1","state":0,"address":"","type":"thermostat","id":"kwn8ehikir1riwgwtteb2b","states":{"0":"Off","1":"On"},"sv":150,"pv":112.8}]}')
-    },
-    {
-      index: 2,
-      name: 'Set sparge temp',
-      id: guid(),
-      comments: 'Sets the sparge temp to 173',
-      config: JSON.parse('{"id":"k2gweii8hwerwktt1birnb","name":"Complete","slackWebhook":"https://hooks.slack.com/services/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","slackChannel":"@luke","devices":[{"name":"HLT Valve","controller_address":"2","state":1,"address":"0","type":"onOff","id":"kebi2wrtr1tiw8ikbghwen","states":{"0":"Closed","1":"Open"}},{"name":"HLT Divert","controller_address":"2","state":0,"address":"1","type":"divert","id":"iwieb81ktkbwrwi2nrehgt","states":{"0":"Boil","1":"Mash"}},{"name":"RIMS Divert","controller_address":"2","state":1,"address":"2","type":"divert","id":"gwbnkreerk8iwi12withbt","states":{"0":"Boil","1":"RIMS"}},{"name":"RIMS Pump","controller_address":"2","state":1,"address":"3","type":"pump","id":"rwtiebb2iwwtikk1nreg8h","states":{"0":"Off","1":"On"}},{"name":"RIMS Thermostat","controller_address":"1","state":0,"address":"","type":"thermostat","id":"kwn8ehikir1riwgwtteb2b","states":{"0":"Off","1":"On"},"sv":150,"pv":112.8}]}')
-    },
-    {
-      index: 0,
-      name: 'Start',
-      id: guid(),
-      comments: 'Just the first step',
-      config: JSON.parse('{"id":"k2gweii8hwerwktt1birnb","name":"Complete","slackWebhook":"https://hooks.slack.com/services/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","slackChannel":"@luke","devices":[{"name":"HLT Valve","controller_address":"2","state":1,"address":"0","type":"onOff","id":"kebi2wrtr1tiw8ikbghwen","states":{"0":"Closed","1":"Open"}},{"name":"HLT Divert","controller_address":"2","state":0,"address":"1","type":"divert","id":"iwieb81ktkbwrwi2nrehgt","states":{"0":"Boil","1":"Mash"}},{"name":"RIMS Divert","controller_address":"2","state":1,"address":"2","type":"divert","id":"gwbnkreerk8iwi12withbt","states":{"0":"Boil","1":"RIMS"}},{"name":"RIMS Pump","controller_address":"2","state":1,"address":"3","type":"pump","id":"rwtiebb2iwwtikk1nreg8h","states":{"0":"Off","1":"On"}},{"name":"RIMS Thermostat","controller_address":"1","state":0,"address":"","type":"thermostat","id":"kwn8ehikir1riwgwtteb2b","states":{"0":"Off","1":"On"},"sv":150,"pv":112.8}]}')
-    },
-    {
-      index: 1,
-      name: 'First Step',
-      id: guid(),
-      comments: 'Closes the HLT Valve',
-      config: JSON.parse('{"id":"k2gweii8hwerwktt1birnb","name":"Complete","slackWebhook":"https://hooks.slack.com/services/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","slackChannel":"@luke","devices":[{"name":"HLT Valve","controller_address":"2","state":1,"address":"0","type":"onOff","id":"kebi2wrtr1tiw8ikbghwen","states":{"0":"Closed","1":"Open"}},{"name":"HLT Divert","controller_address":"2","state":0,"address":"1","type":"divert","id":"iwieb81ktkbwrwi2nrehgt","states":{"0":"Boil","1":"Mash"}},{"name":"RIMS Divert","controller_address":"2","state":1,"address":"2","type":"divert","id":"gwbnkreerk8iwi12withbt","states":{"0":"Boil","1":"RIMS"}},{"name":"RIMS Pump","controller_address":"2","state":1,"address":"3","type":"pump","id":"rwtiebb2iwwtikk1nreg8h","states":{"0":"Off","1":"On"}},{"name":"RIMS Thermostat","controller_address":"1","state":0,"address":"","type":"thermostat","id":"kwn8ehikir1riwgwtteb2b","states":{"0":"Off","1":"On"},"sv":150,"pv":112.8}]}')
-    },
-    {
-      index: 2,
-      name: 'Set sparge temp',
-      id: guid(),
-      comments: 'Sets the sparge temp to 173',
-      config: JSON.parse('{"id":"k2gweii8hwerwktt1birnb","name":"Complete","slackWebhook":"https://hooks.slack.com/services/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","slackChannel":"@luke","devices":[{"name":"HLT Valve","controller_address":"2","state":1,"address":"0","type":"onOff","id":"kebi2wrtr1tiw8ikbghwen","states":{"0":"Closed","1":"Open"}},{"name":"HLT Divert","controller_address":"2","state":0,"address":"1","type":"divert","id":"iwieb81ktkbwrwi2nrehgt","states":{"0":"Boil","1":"Mash"}},{"name":"RIMS Divert","controller_address":"2","state":1,"address":"2","type":"divert","id":"gwbnkreerk8iwi12withbt","states":{"0":"Boil","1":"RIMS"}},{"name":"RIMS Pump","controller_address":"2","state":1,"address":"3","type":"pump","id":"rwtiebb2iwwtikk1nreg8h","states":{"0":"Off","1":"On"}},{"name":"RIMS Thermostat","controller_address":"1","state":0,"address":"","type":"thermostat","id":"kwn8ehikir1riwgwtteb2b","states":{"0":"Off","1":"On"},"sv":150,"pv":112.8}]}')
-    },
-    {
-      index: 0,
-      name: 'Start',
-      id: guid(),
-      comments: 'Just the first step',
-      config: JSON.parse('{"id":"k2gweii8hwerwktt1birnb","name":"Complete","slackWebhook":"https://hooks.slack.com/services/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","slackChannel":"@luke","devices":[{"name":"HLT Valve","controller_address":"2","state":1,"address":"0","type":"onOff","id":"kebi2wrtr1tiw8ikbghwen","states":{"0":"Closed","1":"Open"}},{"name":"HLT Divert","controller_address":"2","state":0,"address":"1","type":"divert","id":"iwieb81ktkbwrwi2nrehgt","states":{"0":"Boil","1":"Mash"}},{"name":"RIMS Divert","controller_address":"2","state":1,"address":"2","type":"divert","id":"gwbnkreerk8iwi12withbt","states":{"0":"Boil","1":"RIMS"}},{"name":"RIMS Pump","controller_address":"2","state":1,"address":"3","type":"pump","id":"rwtiebb2iwwtikk1nreg8h","states":{"0":"Off","1":"On"}},{"name":"RIMS Thermostat","controller_address":"1","state":0,"address":"","type":"thermostat","id":"kwn8ehikir1riwgwtteb2b","states":{"0":"Off","1":"On"},"sv":150,"pv":112.8}]}')
-    },
-    {
-      index: 1,
-      name: 'First Step',
-      id: guid(),
-      comments: 'Closes the HLT Valve',
-      config: JSON.parse('{"id":"k2gweii8hwerwktt1birnb","name":"Complete","slackWebhook":"https://hooks.slack.com/services/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","slackChannel":"@luke","devices":[{"name":"HLT Valve","controller_address":"2","state":1,"address":"0","type":"onOff","id":"kebi2wrtr1tiw8ikbghwen","states":{"0":"Closed","1":"Open"}},{"name":"HLT Divert","controller_address":"2","state":0,"address":"1","type":"divert","id":"iwieb81ktkbwrwi2nrehgt","states":{"0":"Boil","1":"Mash"}},{"name":"RIMS Divert","controller_address":"2","state":1,"address":"2","type":"divert","id":"gwbnkreerk8iwi12withbt","states":{"0":"Boil","1":"RIMS"}},{"name":"RIMS Pump","controller_address":"2","state":1,"address":"3","type":"pump","id":"rwtiebb2iwwtikk1nreg8h","states":{"0":"Off","1":"On"}},{"name":"RIMS Thermostat","controller_address":"1","state":0,"address":"","type":"thermostat","id":"kwn8ehikir1riwgwtteb2b","states":{"0":"Off","1":"On"},"sv":150,"pv":112.8}]}')
-    },
-    {
-      index: 2,
-      name: 'Set sparge temp',
-      id: guid(),
-      comments: 'Sets the sparge temp to 173',
       config: JSON.parse('{"id":"k2gweii8hwerwktt1birnb","name":"Complete","slackWebhook":"https://hooks.slack.com/services/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","slackChannel":"@luke","devices":[{"name":"HLT Valve","controller_address":"2","state":1,"address":"0","type":"onOff","id":"kebi2wrtr1tiw8ikbghwen","states":{"0":"Closed","1":"Open"}},{"name":"HLT Divert","controller_address":"2","state":0,"address":"1","type":"divert","id":"iwieb81ktkbwrwi2nrehgt","states":{"0":"Boil","1":"Mash"}},{"name":"RIMS Divert","controller_address":"2","state":1,"address":"2","type":"divert","id":"gwbnkreerk8iwi12withbt","states":{"0":"Boil","1":"RIMS"}},{"name":"RIMS Pump","controller_address":"2","state":1,"address":"3","type":"pump","id":"rwtiebb2iwwtikk1nreg8h","states":{"0":"Off","1":"On"}},{"name":"RIMS Thermostat","controller_address":"1","state":0,"address":"","type":"thermostat","id":"kwn8ehikir1riwgwtteb2b","states":{"0":"Off","1":"On"},"sv":150,"pv":112.8}]}')
     }
   ]
 }
 
 
-let proc = new Vue({
+let p = new Vue({
   el: '#procedures',
   components: {
     'main-navbar': NavBarComponent,
   },
   data: {
     procSelect: '',
+    config: {},
     procs: [test_proc],
     proc: test_proc,
-    step: {},
+    activeStep: {},
     showTitleInput: false,
     showCommentsInput: false
   },
@@ -129,15 +38,18 @@ let proc = new Vue({
       this.config = config
     },
 
-    activateStepCard(step) {
+    activateStepCard() {
       activeClass = 'uk-background-primary uk-light'
       $('.step-card').removeClass(activeClass);
-      $('#' + step.id + 'Card').addClass(activeClass);
+      $('#' + this.activeStep.id + 'Card').addClass(activeClass);
     },
 
     selectStep(step) {
-      this.step = step;
-      this.activateStepCard(step);
+      this.activeStep = step;
+      setTimeout(() => {
+        this.activateStepCard();
+        $('#stepTitleInput').focus();
+      }, 100);
     },
 
     selectProc(id) {
@@ -150,10 +62,52 @@ let proc = new Vue({
     saveProc() {
       console.log('saved')
     },
+
+    addStep() {
+      prevStep = JSON.parse(JSON.stringify(this.proc.steps[this.proc.steps.length - 1]));
+      step = {
+        index: prevStep.index + 1,
+        tools: {
+          delay: {
+            minutes: '',
+            seconds: ''
+          },
+        },
+        name: '',
+        id: guid(),
+        comments: '',
+        config: prevStep.config
+      };
+
+      this.proc.steps.push(step);
+
+      this.showCommentsInput = true;
+      this.showTitleInput = true;
+
+      this.selectStep(step);
+    },
+
+    reindex() {
+      cards = this.sortable.children()
+      cards.each((index, card) => {
+        stepId = card.id.slice(0, 36)
+        this.proc.steps.forEach(step => {
+          if (step.id == stepId) {
+            step.index = index;
+          }
+        });
+      })
+
+    },
   },
   watch: {
     procSelect: function() {
       this.selectProc(this.procSelect);
     }
+  },
+  mounted() {
+    this.sortable = $('#stepCardColumn').on('moved', () => {
+      this.reindex();
+    })
   }
 })
