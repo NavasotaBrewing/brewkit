ccfc9ed988e4394c41861ba7a724d3cfdcbdaf94
llamicron <llamicron@gmail.com>
2019-09-23 10:51:02 -0500

    Adds addDeviceCard but there's an update issue

---
+++ src/components/AddDeviceCard.vue
@@ -0,0 +1,120 @@
+<template>
+  <Card :title="'New Device'" :type="'tertiary'">
+    <div class="uk-margin">
+      <label for="newDeviceDriver">Driver</label>
+      <select name="driver" id="newDeviceDriver" v-model="newDevice.driver" class="uk-select">
+        <option value disabled>-- Driver --</option>
+        <option value="STR1">STR1</option>
+        <option value="Omega">Omega</option>
+      </select>
+    </div>
+
+    <div class="uk-margin">
+      <label for="newDeviceRTU">RTU</label>
+      <select v-model="newDevice.rtu" id="newDeviceRTU" class="uk-select">
+        <option value disabled>-- RTU --</option>
+        <option v-for="rtu in rtus" :key="rtu.id" :value="rtu.id">{{ rtu.name }}</option>
+      </select>
+    </div>
+
+    <div class="uk-margin">
+      <label for="newDeviceName">Name</label>
+      <input
+        type="text"
+        v-model="newDevice.name"
+        placeholder="Name"
+        class="uk-input"
+        id="newDeviceName"
+      />
+    </div>
+
+    <div v-if="newDevice.driver == 'STR1'" class="uk-margin">
+      <label for="newDeviceAddr">Address</label>
+      <input
+        type="text"
+        v-model="newDevice.addr"
+        placeholder="Address"
+        class="uk-input"
+        id="newDeviceAddr"
+      />
+    </div>
+
+    <div class="uk-margin">
+      <label for="newDeviceControllerAddr">Controller Address</label>
+      <input
+        type="text"
+        v-model="newDevice.controller_addr"
+        placeholder="Controller Address"
+        class="uk-input"
+        id="newDeviceControllerAddr"
+      />
+    </div>
+
+    <div class="uk-margin">
+      <button @click="addDevice" class="uk-button button-tertiary">Add</button>
+    </div>
+  </Card>
+</template>
+
+<script>
+import Card from "@/components/Card.vue";
+
+function uuid() {
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function(c) {
+    var r = (Math.random() * 16) | 0,
+      v = c == "x" ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
+}
+
+function empty(thing) {
+  return thing == "";
+}
+
+export default {
+  name: "AddDeviceCard",
+  props: {
+    rtus: {
+      type: Array,
+      required: true
+    }
+  },
+  components: {
+    Card
+  },
+  data() {
+    return {
+      newDevice: {}
+    };
+  },
+  methods: {
+    notify(msg, status = "") {
+      this.$parent.notify(msg, status);
+    },
+
+    addDevice() {
+      if (
+        empty(this.newDevice.name) ||
+        empty(this.newDevice.driver) ||
+        empty(this.newDevice.controller_addr || empty(this.newDevice.rtu))
+      ) {
+        this.notify("Please fill in all required fields", "danger");
+        return;
+      }
+
+      let vessel = {};
+      Object.assign(vessel, this.newDevice);
+
+      vessel.id = uuid();
+
+      this.newDevice.name = "";
+      this.newDevice.driver = "";
+      this.newDevice.rtu = "";
+      this.newDevice.controller_addr = "";
+      this.newDevice.addr = "";
+
+      this.$emit("newDevice", vessel);
+    }
+  }
+};
+</script>
--- src/views/Configure.vue
+++ src/views/Configure.vue
@@ -151,47 +151,7 @@
         <div uk-grid>
           <!-- New Device Card -->
           <div class="uk-width-1-3@m">
-
-            <Card :title="'New Device'" :type="'tertiary'">
-
-              <div class="uk-margin">
-                <label for="newDeviceDriver">Driver</label>
-                <select name="driver" id="newDeviceDriver" v-model="newDevice.driver" class="uk-select">
-                  <option value="" disabled>-- Driver --</option>
-                  <option value="STR1">STR1</option>
-                  <option value="Omega">Omega</option>
-                </select>
-              </div>
-
-              <div class="uk-margin">
-                <label for="newDeviceRTU">RTU</label>
-                <select v-model="newDevice.rtu" id="newDeviceRTU" class="uk-select">
-                  <option value="" disabled>-- RTU --</option>
-                  <option v-for="rtu in config.RTUs" :key="rtu.id" :value="rtu.id">{{ rtu.name }}</option>
-                </select>
-              </div>
-
-              <div class="uk-margin">
-                <label for="newDeviceName">Name</label>
-                <input type="text" v-model="newDevice.name" placeholder="Name" class="uk-input" id="newDeviceName">
-              </div>
-
-              <div v-if="newDevice.driver == 'STR1'" class="uk-margin">
-                <label for="newDeviceAddr">Address</label>
-                <input type="text" v-model="newDevice.addr" placeholder="Address" class="uk-input" id="newDeviceAddr">
-              </div>
-
-              <div class="uk-margin">
-                <label for="newDeviceControllerAddr">Controller Address</label>
-                <input type="text" v-model="newDevice.controller_addr" placeholder="Controller Address" class="uk-input" id="newDeviceControllerAddr">
-              </div>
-
-              <div class="uk-margin">
-                <button @click="addDevice" class="uk-button button-tertiary">Add</button>
-              </div>
-
-            </Card>
-
+            <AddDeviceCard @newDevice="addDevice($event)" :rtus="config.RTUs"></AddDeviceCard>
           </div>
 
           <!-- All Devices -->
@@ -218,20 +178,10 @@ import Card from "@/components/Card.vue";
 import Confirmation from "@/components/Confirmation.vue";
 import AddRTUCard from '@/components/AddRTUCard.vue';
 import RTU from '@/components/RTU.vue';
-// import AddDeviceCard from '@/components/AddDeviceCard.vue';
+import AddDeviceCard from '@/components/AddDeviceCard.vue';
 import Device from '@/components/Device.vue';
 import Slack from '@/slack.js';
 
-function uuid() {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
-    return v.toString(16);
-  });
-}
-
-function empty(thing) {
-  return thing == '';
-}
 
 export default {
   name: "Configure",
@@ -240,7 +190,7 @@ export default {
     Confirmation,
     AddRTUCard,
     RTU,
-    // AddDeviceCard,
+    AddDeviceCard,
     Device
   },
   data() {
@@ -249,14 +199,6 @@ export default {
       configs: [],
       newConfigName: "",
       configSelect: -1,
-      newDevice: {
-        driver: "",
-        rtu: "",
-        name: "",
-        addr: "",
-        controller_addr: "",
-        id: "",
-      }
     };
   },
   computed: {
@@ -365,38 +307,17 @@ export default {
       this.notify("RTU added", 'success');
     },
 
-    addDevice() {
-      if (
-        empty(this.newDevice.name) ||
-        empty(this.newDevice.driver) ||
-        empty(this.newDevice.controller_addr ||
-        empty(this.newDevice.rtu)))
-      {
-        this.notify("Please fill in all required fields", 'danger');
+    addDevice(device) {
+      let rtu = this.config.RTUs.filter(r => r.id == device.rtu)[0];
+      if (!rtu) {
+        this.notify("Could not add device", 'danger');
         return;
       }
-
-      let vessel = {};
-      Object.assign(vessel, this.newDevice);
-
-      vessel.id = uuid();
-
-      let rtu = this.config.RTUs.filter(r => r.id == vessel.rtu)[0];
-
-      if (!rtu.devices) {
-        rtu.devices = [];
-      }
-
-      rtu.devices.push(vessel);
-
-      this.newDevice.name = "";
-      this.newDevice.driver = "";
-      this.newDevice.rtu = "";
-      this.newDevice.controller_addr = "";
-      this.newDevice.addr = "";
-
+      if (!rtu.devices) { rtu.devices = [] };
+      rtu.devices.push(device);
       this.notify("Device added", 'success');
     }
+
   },
   watch: {
     configSelect: function() {
